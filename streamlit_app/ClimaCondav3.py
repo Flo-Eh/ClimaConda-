fd6cac641142e1c63df5d7158552225ecaebd58b
Flo-Eh <116956039+Flo-Eh@users.noreply.github.com>
2023-01-16 11:52:01 +0100

    Update ClimaCondav3.py

--- streamlit_app/ClimaCondav3.py
+++ streamlit_app/ClimaCondav3.py
@@ -1696,13 +1696,12 @@ if page == pages[6]:
         Whereas ARIMA and machine learning models have regular performances when they are run several times, we observed that the performances of the deep learning models were quite erratic.
         
 	Deep learning models are probably not the best choice for our problematic for several reasons:
-	
+		
 	They are time consuming to implement as there is no grid search function
 	The parameters used for one series might not be adapted for the others
 	They are not performant or robust with so few data
 
-	The limited size of our dataset was the main difficulty in our project: publication of emissions data is quite recent and uses to be on a yearly basis. For France, monthly data are only available since 2019.
-             
+        The limited size of our dataset was the main difficulty in our project: publication of emissions data is quite recent and uses to be on a yearly basis. For France, monthly data are only available since 2019.    
 	
         """
     )
